1d9047b1fef77a23cb43f7513eecb54d1d3e032d
sandragmuer <69652320+sandragmuer@users.noreply.github.com>
2020-10-02 23:36:12 +0200

    Added search engine

--- src/app.js
+++ src/app.js
@@ -42,8 +42,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "9a086176ea63e575b3eb398ef6d24568";
-let city = "Walenstadt";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "9a086176ea63e575b3eb398ef6d24568";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Walenstadt");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
